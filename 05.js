fc2b0356066ee2ff5ac34bc5eef9ad0913b52d9b
Kenta Baron-Furuyama <kentabaronfuruyama@gmail.com>
2020-06-10 17:45:21 +0900

    prototytpe

---
+++ 05.js
@@ -0,0 +1,19 @@
+/*
+A Beginner's Guide to JavaScript's Prototype
+*/
+
+/*
+"prototype" or `prototype`:
+- property that every function in JS has
+- allows sharing of methods across all instances of functions
+- `getPrototypeOf` allows get instance of prototype
+
+`for (let key in <Obj>)` allows iteration through all fields, including functions in accompanying prototype
+
+`hasOwnProperty` checks whether it's in itself
+
+arrow functions don't have `this` or `prototype`
+*/
+
+
+
